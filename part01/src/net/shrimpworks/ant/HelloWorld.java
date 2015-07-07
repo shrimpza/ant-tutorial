53a50685de0b69fa9262b66589cb22c4ea02cef6
Kenneth Watson <git@shrimpworks.za.net>
2015-07-07 17:58:19 +0200

    commit part 1

---
+++ part01/src/net/shrimpworks/ant/HelloWorld.java
@@ -0,0 +1,16 @@
+package net.shrimpworks.ant;
+
+/**
+ * A small utility class which produces Hello World strings.
+ */
+public class HelloWorld {
+
+	/**
+	 * Creates a Hello World string.
+	 *
+	 * @return the String "Hello World!"
+	 */
+	public String helloWorld() {
+		return "Hello World!";
+	}
+}
