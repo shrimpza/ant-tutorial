be2358d1955496c2ddf17bd8100e8538c8f37933
Kenneth Watson <git@shrimpworks.za.net>
2015-07-15 17:27:18 +0200

    create part 3

---
+++ part03/src/net/shrimpworks/ant/Main.java
@@ -0,0 +1,15 @@
+package net.shrimpworks.ant;
+
+/**
+ * Application's main class.
+ */
+public class Main {
+
+	public static void main(String... args) {
+		// create a hello world instance...
+		HelloWorld hw = new HelloWorld();
+
+		// ... and use it to print Hello World.
+		System.out.println(hw.helloWorld());
+	}
+}
