fc0894c647ac35cfb63d1bdd807785cd73757a2f
Kenneth Watson <git@shrimpworks.za.net>
2015-07-16 19:57:05 +0200

    make use of downloaded dependencies

--- part03/src/net/shrimpworks/ant/Main.java
+++ part03/src/net/shrimpworks/ant/Main.java
@@ -1,15 +1,20 @@
 package net.shrimpworks.ant;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Application's main class.
  */
 public class Main {
 
 	public static void main(String... args) {
+		Logger logger = LoggerFactory.getLogger(Main.class);
+
 		// create a hello world instance...
 		HelloWorld hw = new HelloWorld();
 
 		// ... and use it to print Hello World.
-		System.out.println(hw.helloWorld());
+		logger.info(hw.helloWorld());
 	}
 }
